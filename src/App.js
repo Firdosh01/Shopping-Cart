774c8732db249ba4332089172d9fb9aacb3c8a71
Firdosh01 <firdosh7392@gmail.com>
2023-05-03 03:48:21 -0500

    Shopping Cart

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+
+function App() {
+  return (
+    <div>
+      Shopping Cart
+      </div>
+      
+  );
+}
+
+export default App;
+
+
