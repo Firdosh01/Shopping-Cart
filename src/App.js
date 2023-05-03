1c51d4b8c88bc118ee0990c8bb06d3ef4de44198
Firdosh01 <firdosh7392@gmail.com>
2023-05-03 04:00:59 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 
 function App() {
   return (
     <div>
-      Shopping Cart
+      <div>
+      <Navbar />
+      </div>
+
+      <Routes>
+        
+        <Route path='/' element= { <Home />} />
+        <Route path='/cart' element= { <Cart/>} />
+
+      </Routes>
       </div>
       
   );
 }
 
 export default App;
-
 
---
+++ src/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cart() {
+
+
+  return (
+
+    <div>This is Cart Page</div>
+  )
+}
+
+export default Cart
