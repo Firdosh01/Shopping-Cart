7766409f681d912f3e11f7043a7e6751f706eda6
Firdosh01 <firdosh7392@gmail.com>
2023-05-06 10:30:25 -0500

    product cart length logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {FaShoppingCart} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 function Navbar() {
+
+  const {cart} = useSelector( (state) => state);
+  
   return (
+    
     <div>
       <nav className='flex items-center justify-between h-20 max-w-6xl mx-auto'>
         
@@ -21,8 +26,12 @@ function Navbar() {
           </NavLink>
           
           <NavLink to= "/cart">
-            <div>
-            < FaShoppingCart />                
+            <div className='relative'>
+            < FaShoppingCart className='text-2xl' /> 
+            {
+              cart.length > 0 &&
+              <span className='absolute flex items-center justify-center w-5 h-5 text-xs text-white bg-green-600 rounded-full -top-1 -right-2 animate-bounce'>{cart.length}</span>    
+            }            
             </div>
           
           </NavLink>
