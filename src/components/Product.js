1fb9f0be8d2e77a673d1dc3f10d933b203d00b81
Firdosh01 <firdosh7392@gmail.com>
2023-05-09 04:08:03 -0500

    product card styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -24,37 +24,39 @@ function Product( {items}) {
 
   
   return (
-    <div>
+    <div className='flex flex-col items-center justify-between gap-3 p-4 mt-10 ml-5 transition duration-200 ease-in hover:scale-110 rounded-xl outline'>
       <div>
-        <p>{items.title}</p>
+        <p className='text-gray-700 '>{items.title.split( " ").slice(0,3).join( " ") + "..."}</p>
       </div>
 
       <div>
-        <p>{items.description}</p>
+        <p className='w-40 font-normal text-gray-400 text-[10px] text-left'>{items.description.split( " ").slice(0,10).join( " ") + "..."}</p>
       </div>
 
-      <div>
-        <img src={items.image} alt="" />
+      <div className='h-[180px]'>
+        <img src={items.image} alt="" className='w-full h-full' />
       </div>
 
-      <div>
-        <p>{items.price}</p>
+      <div className='flex justify-between gap-12'>
+        <div>
+        <p className='font-semibold text-green-600'>${items.price}</p>
       </div>
 
       {
         cart.some( (p) => p.id == items.id) ?
         (
-          <button onClick={removeFromCart}>
+          <button className='font-semibold text-gray-700 border-2 border-gray-700 rounded-full text-[12px] p-1 px-3 uppercase hover:bg-gray-700 hover:text-white' onClick={removeFromCart}>
             Remove Item
           </button>
         ) :
         (
-          <button onClick={addToCart}>
+          <button className='font-semibold text-gray-700 border-2 border-gray-700 rounded-full text-[12px] p-1 px-3 uppercase hover:bg-gray-700 hover:text-white' onClick={addToCart}>
             Add To Cart
           </button>
         )
       }
 
+    </div>
     </div>
   )
 }
